954ec5a2dbdae6b219e817855d93238f726b29cd
Alex <Alex@alex-desktop>
2015-03-26 14:31:11 +0000

    Adding GA classes

---
+++ KeyphraseExtractor/src/ga/algorithm/Individual.java
@@ -0,0 +1,129 @@
+package ga.algorithm;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author Alexander Brown - 10019841
+ */
+public class Individual {
+
+    private double[] gene;
+    private int fitness;
+    private Random rn;
+
+    public Individual(int size) {
+        this.gene = new double[size];
+        this.rn = new Random();
+        this.populate();
+        this.fitness = 0;
+    }
+
+    public Individual(double[] gene) {
+        this.gene = gene;
+        this.fitness = 0;
+        this.rn = new Random();
+    }
+
+    private void populate() {
+        for (int i = 0; i < gene.length; i++) {
+            if (i % 3 == 0) {
+                gene[i] = rn.nextInt(19);
+            } else {
+                gene[i] = rn.nextDouble();
+            }
+        }
+    }
+
+    public Individual[] crossover(Individual parent2) {
+        Individual parent1 = this;
+        int crossoverPoint = generateCrossOverPoint();
+        double[] g1, g2;
+
+        g1 = new double[gene.length];
+        g2 = new double[gene.length];
+
+        for (int i = 0; i < gene.length; i++) {
+            if (i < crossoverPoint) {
+                g1[i] = parent1.getGene()[i];
+                g2[i] = parent2.getGene()[i];
+            } else {
+                g1[i] = parent2.getGene()[i];
+                g2[i] = parent1.getGene()[i];
+            }
+        }
+
+        Individual[] children = new Individual[2];
+        children[0] = new Individual(g1);
+        children[1] = new Individual(g2);
+
+        return children;
+    }
+
+    private int generateCrossOverPoint() {
+        int ub = gene.length - 1;
+        int lb = 1;
+        int point = rn.nextInt(ub - lb) + lb;
+        return point;
+    }
+
+    public Individual mutate(double mutationRate) {
+        double[] tempGene = this.gene;
+        for (int i = 0; i < tempGene.length; i++) {
+            if (rn.nextDouble() <= mutationRate) {
+                if (i % 3 == 0) {
+                    tempGene[i] = rn.nextInt(19);
+                } else {
+                    double modifier;
+                    if (rn.nextBoolean()) {
+                        modifier = rn.nextDouble() * 0.1;
+                    } else {
+                        modifier = -(rn.nextDouble() * 0.1);
+                    }
+                    if ((tempGene[i] + modifier) > 1) {
+                        tempGene[i] = 1;
+                    } else if ((modifier + tempGene[i]) < 0) {
+                        tempGene[i] = 0;
+                    } else {
+                        tempGene[i] += modifier;
+                    }
+                }
+
+            }
+        }
+        return new Individual(tempGene);
+    }
+
+    public String displayGene() {
+        String out = "";
+        for (int i = 0; i < gene.length; i++) {
+            out = out.concat(" " + Double.toString(gene[i]));
+        }
+        return out;
+    }
+
+    public double[] getGene() {
+        return gene;
+    }
+
+    public ArrayList<Double> getGeneArrayList() {
+        ArrayList<Double> al = new ArrayList<>();
+        for (double g : gene) {
+            al.add(g);
+        }
+        return al;
+    }
+
+    public void setGene(double[] gene) {
+        this.gene = gene;
+    }
+
+    public int getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(int fitness) {
+        this.fitness = fitness;
+    }
+}
---
+++ KeyphraseExtractor/src/ga/algorithm/Population.java
@@ -0,0 +1,144 @@
+package ga.algorithm;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author Alexander Brown - 10019841
+ */
+public class Population {
+
+    //private Individual[] population;
+
+    private ArrayList<Individual> population;
+    private Random rn;
+
+    public Population(int pop_size, int gene_size) {
+        rn = new Random();
+        population = new ArrayList<>(pop_size);
+        for (int i = 0; i < pop_size; i++) {
+            population.add(i, new Individual(gene_size));
+        }
+    }
+
+    public Population() {
+        rn = new Random();
+        population = new ArrayList<>();
+    }
+
+    public void addIndividual(Individual ind) {
+        population.add(ind);
+    }
+
+    public Individual getIndividual(int index) {
+        return population.get(index);
+    }
+
+    public void setIndividual(Individual ind, int index) {
+        population.set(index, ind);
+    }
+
+    public Individual removeIndividual(int index) {
+        return population.remove(index);
+    }
+
+    public void runFitnessAll() {
+        for (Individual ind : population) {
+            //ind.updateFitness();
+        }
+    }
+
+    public double calculateFitnessMean() {
+        double mean = this.calculateFitnessSum() / population.toArray().length;
+        return mean;
+    }
+
+    public int calculateFitnessSum() {
+        int sum = 0;
+        for (Individual ind : population) {
+            sum += ind.getFitness();
+        }
+        return sum;
+    }
+
+    public Individual getFittestIndividual() {
+        int best = 0;
+        Individual bestInd = null;
+        for (Individual ind : population) {
+            if (ind.getFitness() > best) {
+                best = ind.getFitness();
+                bestInd = ind;
+            }
+        }
+        return bestInd;
+    }
+
+    public int calculateBestFitness() {
+        int best = 0;
+        for (Individual ind : population) {
+            if (ind.getFitness() > best) {
+                best = ind.getFitness();
+            }
+        }
+        return best;
+    }
+
+    public ArrayList<Individual> getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Population newPop) {
+        this.population = newPop.getPopulation();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getSize() {
+        return population.toArray().length;
+    }
+
+    public Population selectParents() {
+        Population parents = new Population();
+        for (int i = 0; i < this.getSize(); i++) {
+            Individual parent1 = population.get(rn.nextInt(this.getSize() - 1));
+            Individual parent2 = population.get(rn.nextInt(this.getSize() - 1));
+            if (parent1.getFitness() > parent2.getFitness()) {
+                parents.addIndividual(parent1);
+            } else {
+                parents.addIndividual(parent2);
+            }
+        }
+        return parents;
+    }
+
+    public void combine(double rate) {
+        Population childPop = new Population();
+        while (!population.isEmpty()) {
+            Individual ind1 = population.remove(rn.nextInt(this.getSize()));
+            Individual ind2 = population.remove(rn.nextInt(this.getSize()));
+
+            if (rn.nextDouble() <= rate) { // Crossover chance
+                Individual[] childPair = ind1.crossover(ind2);
+                childPop.addIndividual(childPair[0]);
+                childPop.addIndividual(childPair[1]);
+            } else {
+                childPop.addIndividual(ind1);
+                childPop.addIndividual(ind2);
+            }
+        }
+        this.setPopulation(childPop);
+    }
+
+    public void mutatePopulation(double rate) {
+        for (Individual ind : population) {
+            ind = ind.mutate(rate);
+        }
+    }
+
+    public void clear() {
+        population.clear();
+    }
+}
