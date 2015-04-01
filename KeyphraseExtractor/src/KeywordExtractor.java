0b9d26ee78e3f65d8a63638e097a361cba322ac2
Alex <Alex@alex-desktop>
2015-04-01 21:21:11 +0100

    Modified how program starts. Added ability to test a given document

---
+++ KeyphraseExtractor/src/KeywordExtractor.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author Alex
+ */
+public class KeywordExtractor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args[0].equalsIgnoreCase("-t")){
+            ExtractorTrainer ext = new ExtractorTrainer();
+            ext.train(args[1]);
+        } else if (args[0].equalsIgnoreCase("-e")){
+            Extractor ex = new Extractor();
+            ex.extract(args[1]);
+        }
+    }
+
+}
